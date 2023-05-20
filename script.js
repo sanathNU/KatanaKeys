04c3510b4fd7d13605cbf0e671d18d46b33b9736
sanathNU <sanathn.upadhyaya@gmail.com>
2023-05-20 22:16:42 +0530

    Just updated the readme

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ async function getRandomQuote() {
     const response = await fetch('http://localhost:5000/quotes');
     const data = await response.json();
     const quotes = data.quotes;
+    console.log("Received quotes",quotes);
     const index = Math.floor(Math.random() * quotes.length);
     return quotes[index];
 }
 
 async function displayQuote() {
   const randomQuote = getRandomQuote();
+  console.log(randomQuote);
   randomQuote.then(quote => {
     quoteContainer.innerText = quote;
   });
 }
+
+// displayQuote()
 
 document.addEventListener('DOMContentLoaded', function() {
   displayQuote();
