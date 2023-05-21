e8775ab5e0a5b0e13817e0a3e3c6b5bbaaf65008
sanathNU <sanathn.upadhyaya@gmail.com>
2023-05-21 18:20:58 +0530

    Running everything from flask, moved to boostrap

--- scrapeQuotes.py
+++ scrapeQuotes.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, flash, request, redirect, url_for, render_template, jsonify, send_from_directory
 from flask_restful import Api, Resource
 import random
 
@@ -17,6 +17,10 @@ class Quotes(Resource):
         return jsonify({'quotes': quotes})
 
 api.add_resource(Quotes, '/quotes')
+
+@app.route('/')
+def index(): 
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
